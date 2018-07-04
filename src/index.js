703c9a06ac7c89f2a8c474fd52c7d8f8dde3c4a8
David Hulmes <dhulmes@gmail.com>
2018-07-04 21:18:10 +0100

    stargame complete

--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import { Observable } from "rxjs";
+import { Observable, BehaviorSubject } from "rxjs";
+import { RSA_X931_PADDING } from "constants";
 
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d');
+const ScoreSubject = new BehaviorSubject(0);
+const score = ScoreSubject.scan((prev,cur)=> prev + cur,0);
+const SCORE_INCREASE = 10;
+
 document.body.appendChild(canvas);
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
@@ -48,6 +53,8 @@ function renderScene(actors){
   paintStars(actors.stars);
   paintSpaceShip(actors.spaceship.x, actors.spaceship.y);
   paintEnemies(actors.enemies);
+  paintHeroShots(actors.heroShots, actors.enemies);
+  paintScore(actors.score);
 }
 
 function paintSpaceShip(x,y){
@@ -68,14 +75,31 @@ const SpaceShip = mouseMove
     })
 
 const ENEMY_FREQ = 1500;
+const ENEMY_SHOOTING_FREQ = 750;
+
+function isVisible(obj){
+  return obj.x > -40 &&
+    obj.x < canvas.width + 40 &&
+    obj.y > -40 &&
+    obj.y < canvas.height + 40;
+}
 const Enemies$ = Observable.interval(ENEMY_FREQ).scan(enemyArray=>{
   const enemy = {
     x: parseInt(Math.random() * canvas.width, 10),
-    y: -30
+    y: -30,
+    shots: []
   };
 
+  Observable.interval(ENEMY_SHOOTING_FREQ).subscribe(()=>{
+    if(!enemy.isDead){
+      enemy.shots.push({x: enemy.x, y: enemy.y});
+    }
+    enemy.shots = enemy.shots.filter(isVisible);
+  })
+
   enemyArray.push(enemy);
-  return enemyArray;
+  return enemyArray.filter(isVisible)
+    .filter(enemy=>!(enemy.isDead && enemy.shots.length===0));
 }, []);
 
 function getRandomInt(min, max){
@@ -86,16 +110,111 @@ function paintEnemies(enemies){
   enemies.forEach(enemy=>{
     enemy.y+= 5,
     enemy.x += getRandomInt(-15,15);
-    drawTriangle(enemy.x, enemy.y, 20, "#00ff00", 'down');
+
+    if(!enemy.isDead){
+      drawTriangle(enemy.x, enemy.y, 20, "#00ff00", 'down');
+    }
+
+    enemy.shots.forEach(shot=>{
+      shot.y+=SHOOTING_SPEED;
+      drawTriangle(shot.x, shot.y, 5, '#00ffff', 'down');
+    })
   })
 }
 
-const Game = Observable.combineLatest(StarStream$, SpaceShip, Enemies$, (
+function paintScore(score){
+  ctx.fillStyle = '#ffffff';
+  ctx.font = 'bold 26px sans-serif',
+  ctx.fillText(`Score: ${score}`, 40, 43);
+}
+
+function collision(target1, target2){
+  let collision = (
+    target1.x>target2.x-20 &&
+    target1.x<target2.x+20 &&
+    (target1.y>target2.y-20 && target1.y<target2.y+20)
+  );
+  if(collision){
+    console.log('collision detected');
+  }
+  return collision;
+}
+
+const playerFiring = Observable
+  .merge(
+    Observable.fromEvent(canvas, 'click'),
+    Observable.fromEvent(document, 'keydown')
+      .filter(evt=> evt.keyCode===32)
+  )
+  .startWith({})
+  .sampleTime(200)
+  .timestamp();
+
+const HeroShots = Observable
+  .combineLatest(playerFiring, SpaceShip, 
+    (shotEvents, spaceship)=>{
+      return {
+        timestamp: shotEvents.timestamp,
+        x:spaceship.x}
+    }
+  )
+  .distinctUntilChanged( (a,b, c)=>{
+    return a.timestamp===b.timestamp;
+  })
+  .scan(
+    (shotArray, shot)=> {
+      shotArray.push({
+        x:shot.x,
+        y:HERO_Y
+      });
+      return shotArray;
+    },[]
+  );
+
+const SHOOTING_SPEED = 15;
+
+function gameOver(ship, enemies) {
+  return enemies.some(enemy => {
+    if (collision(ship, enemy)) {
+      console.log('GAME OVER');
+      return true;
+    }
+
+    return enemy.shots.some(shot => collision(ship, shot));
+  });
+}
+
+function paintHeroShots(heroShots, enemies){
+  heroShots.forEach(shot=>{
+    for(let l =0; l<enemies.length;l++){
+      const enemy = enemies[l];
+      if(!enemy.isDead && collision(shot, enemy)){
+        ScoreSubject.next(SCORE_INCREASE);
+        enemy.isDead = true;
+        shot.x = shot.y = -100;
+        break;
+      }
+    }
+    shot.y -= SHOOTING_SPEED;
+    drawTriangle(shot.x, shot.y, 5, '#ffff00', 'up');
+  })
+}
+
+
+
+
+
+const Game = Observable.combineLatest(StarStream$, SpaceShip, Enemies$, HeroShots, score, (
   stars,
   spaceship,
-  enemies
+  enemies,
+  heroShots,
+  score
 ) => ({
-  stars,spaceship, enemies
-}));
+  stars,spaceship, enemies, heroShots, score
+}))
+.sampleTime(SPEED)
+.takeWhile(actors=>gameOver(actors.spaceship, actors.enemies)===false);
+
 
 Game.subscribe(renderScene);
